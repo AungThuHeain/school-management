c0108f46f0894c79cca06c8adc7907d818c8bd27
KhineZinThaw39 <khinezinthaw9839@gmail.com>
2024-11-14 22:20:08 +0630

    done

--- resources/views/layouts/dashboard-sidebar.blade.php
+++ resources/views/layouts/dashboard-sidebar.blade.php
@@ -1,5 +1,5 @@
-<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
-    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-green-500 border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+    <div class="h-full px-3 pb-4 overflow-y-auto bg-green-500 dark:bg-gray-800">
        <ul class="space-y-2 font-medium">
           <li>
              <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
@@ -8,7 +8,7 @@
              </a>
           </li>
           <li>
-            <a href="{{route('roles.index')}}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <a href="{{route('roles.index')}}" class=" {{ Request::routeIs('roles.*') ? 'bg-gray-50' : ''}} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                <i class="ri-shield-keyhole-fill"></i>
                <span class="flex-1 ms-3 whitespace-nowrap">Roles</span>
                <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">{{Spatie\Permission\Models\Role::where('school_id',auth()->user()->school_id)->count()}}</span>
